125d160c68ca2a91f003fbf18d6985b6c0540b03
Kevin Depuydt <kevin.dpdt@gmail.com>
2023-03-30 16:31:26 +0200

    move api logic into route file

--- src/pages/api/image-compress.ts
+++ src/pages/api/image-compress.ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import multer from 'multer'
-import imageService from '@services/image'
+import sharp from 'sharp'
+// import imageService from '@services/image'
+// import { Buffer } from 'buffer'
 
 
 type Data = {
@@ -12,7 +14,8 @@ type Data = {
 export const config = {
   api: {
     bodyParser: false
-  }
+  },
+  type: 'experimental-background',
 }
 
 async function parseFiles(
@@ -34,6 +37,35 @@ async function parseFiles(
   return req.files;
 }
 
+const SHARP_CONFIG = {
+  jpeg: { quality: 70 },
+  png: { quality: 70, compressionLevel: 8 },
+  webp: { quality: 70 }
+}
+
+async function compressImage(file: Express.Multer.File): Promise<Buffer> {
+  const image = sharp(file.buffer)
+  const { format } = await image.metadata()
+
+  switch (format) {
+  case 'jpeg':
+  case 'jpg':
+    image.jpeg(SHARP_CONFIG.jpeg)
+    break
+  case 'png':
+    image.png(SHARP_CONFIG.png)
+    break
+  case 'webp':
+    image.webp(SHARP_CONFIG.webp)
+    break
+  default:
+    console.log(`format ${format} not handled for image compression`)
+    break
+  }
+
+  return image.toBuffer()
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
@@ -41,7 +73,8 @@ export default async function handler(
   try {
     const files = await parseFiles(req, res)
     if (files.length > 0) {
-      const buffer = await imageService.compress(files[0])
+      // const buffer = await imageService.compress(files[0])
+      const buffer = await compressImage(files[0])
       return res.status(200).json({ type: files[0].type, base64: buffer.toString('base64') })
     }
 
