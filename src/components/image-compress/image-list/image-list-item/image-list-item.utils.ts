06120388e051c15546acb291a808d920e0dbc7e7
Kevin Depuydt <kevin.dpdt@gmail.com>
2023-03-30 10:56:47 +0200

    fix image download

--- src/components/image-compress/image-list/image-list-item/image-list-item.utils.ts
+++ src/components/image-compress/image-list/image-list-item/image-list-item.utils.ts
@@ -1,15 +1,10 @@
-export async function fileToBase64(file: File): Promise<string> {
+export async function fileToDataUrl(file: File): Promise<string> {
   return new Promise((resolve, reject) => {
     let reader = new FileReader();
     reader.onload = () => resolve(reader.result as string)
     reader.onerror = reject
     reader.readAsDataURL(file)
   })
-}
-
-export async function fileToDataUrl(file: File): Promise<string> {
-  const base64Src = await fileToBase64(file)
-  return `data:${file.type};base64,${base64Src}`
 }
 
 export function compareFilesSize(inputFile: File, outputFile: File): number {
