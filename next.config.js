ad3d067e0d8bbd5b851d906f1bf02623327a19e1
Kevin Depuydt <kevin.dpdt@gmail.com>
2023-04-18 19:01:50 +0200

    Revert "fix: remove data-cy attributes from production build"
    This reverts commit 8896ccb7c89cfd5d113b786bba913a8bc1277c5d.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    reactRemoveProperties: { properties: ['^data-cy$'] },
-  },
 }
 
 module.exports = nextConfig
