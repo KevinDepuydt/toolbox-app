c1d9c3d2089cf19143e476b8b7e931fec743e85b
Kevin Depuydt <kevin.dpdt@gmail.com>
2023-06-13 18:03:28 +0200

    feat: handle button styling options

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,17 +2,21 @@ import React from 'react'
 import classNames from 'classnames'
 import styles from './button.module.css'
 
+type ButtonStyle = 'default' | 'primary' | 'success' | 'danger'
 
 interface ButtonProps {
+  style?: ButtonStyle
   label?: string
   children?: React.ReactNode
   className?: string
+  fluid?: boolean
+  disabled?: boolean
   onClick(event: React.MouseEvent<HTMLElement>): void
 }
 
-export default function Button({ children, label, onClick, className = '' }: ButtonProps) {
+export default function Button({ style = 'default', children, label, onClick, disabled = false, fluid = false, className = '' }: ButtonProps) {
   return (
-    <button className={classNames(styles.green, className)} onClick={onClick}>
+    <button className={classNames(styles[style], { [styles.fluid]: fluid }, className)} onClick={onClick} disabled={disabled}>
       {label}
       {children}
     </button>
